1a7d401f38a6b1d2360860076a9cb5fa7157793c
Ruth Cherono <ruthcheronoofficial@gmail.com>
2024-10-15 18:58:48 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Footer from './Components/Footer'; 
+import Home from './Pages/Home';
+import Products from './Pages/Products';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+      {/* <Navbar /> */} 
+        
+        <main className="main">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main> 
+
+      {/*   <Footer /> */} 
+      </div>
+    </Router>
   );
 }
 
